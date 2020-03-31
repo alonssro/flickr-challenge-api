7a58fc1116d1aff1b3ebbd936b029b63a6ca7bba
Alonso Rodriguez <david.rodriguez@konfio.mx>
2020-03-31 00:12:29 -0600

    Added compodoc and reformated README.md

--- src/dto/flickr-search.dto.ts
+++ src/dto/flickr-search.dto.ts
@@ -1,3 +1,9 @@
+/**
+ * Data transfet object to receive data
+ */
 export class FlickrSearchDto {
+  /**
+   * Text to search on flickr api
+   */
   text: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+/**
+ * Entry point
+ */
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: true });
